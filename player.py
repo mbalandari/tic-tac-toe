b98888b08204c2d2475015a78146bc979354f0aa
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-20 10:31:00 +0330

    first

---
+++ player.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+
+    # next move given a game for all players
+    def game_move(self, game):
+        pass
